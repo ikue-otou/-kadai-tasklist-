1dbd90f4a48a72165b2aca58299f056bd1fc7071
cyacyako.toou <rcmg@outlook.jp>
2020-02-14 12:16:38 +0000

    add column

--- database/migrations/2020_02_13_111812_add_title_to_tasks_table.php
+++ database/migrations/2020_02_13_111812_add_title_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddTitleToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status',10)->change();
+            $table->string('status',10);
         });
     }
     /**
